c0c108617f92e11c7dbfcee0328dae9e776a671e
zadnap <zadnap@users.noreply.github.com>
2024-08-10 22:12:45 +0700

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numb1, numb2) {
+    let sum = 0
+    if (numb1 < 0 || numb2 < 0 || !Number.isInteger(numb1) || !Number.isInteger(numb2)) 
+        return "ERROR"
+    if (numb1 < numb2) {
+        for (i = numb1; i <= numb2; i++) {
+            sum += i
+        }
+    }
+    else if (numb1 > numb2) {
+        for (i = numb2; i <= numb1; i++) {
+            sum += i
+        }
+    }
+    else {
+        sum = numb1
+    }
+    return sum
 };
 
 // Do not edit below this line
