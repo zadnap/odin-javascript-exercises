edc927f713f58e65b47f0b51fcc11f9b70b9d86d
zadnap <zadnap@users.noreply.github.com>
2024-08-10 21:55:20 +0700

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split('')
+    return array.reverse().join('')
 };
 
 // Do not edit below this line
