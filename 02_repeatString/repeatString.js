cfdb1cf0c1b4740896efb347e859b9ece1742aff
zadnap <zadnap@users.noreply.github.com>
2024-08-10 21:51:11 +0700

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, time) {
+    if (time < 0) {
+        return "ERROR"
+    }
+    let finalString = ''
+    for (let i = 0; i < time; i++) {
+        finalString = finalString.concat(string)
+    }
+    return finalString
 };
 
 // Do not edit below this line
