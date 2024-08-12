c10be5e151409537ffb9932acad570c9dd793a5e
zadnap <zadnap@users.noreply.github.com>
2024-08-12 20:06:00 +0700

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentValue) => (total + currentValue), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, currentValue) => total * currentValue)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(numb) {
+  if (numb === 0)
+    return 1
+  else
+	  return numb * factorial(numb - 1)
 };
 
 // Do not edit below this line
