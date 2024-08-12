9064feef67ff2abc8a3fcbbb00dae36c81e4b528
zadnap <zadnap@users.noreply.github.com>
2024-08-12 20:20:07 +0700

    Finish 08_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let refinedString = removeSpacesAndPunctuations(str)
+    return refinedString === refinedString.split('').reverse().join('')
 };
+
+function removeSpacesAndPunctuations(str) {
+    return str.replace(/[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase()
+}
 
 // Do not edit below this line
 module.exports = palindromes;
