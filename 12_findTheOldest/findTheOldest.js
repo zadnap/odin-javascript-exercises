839a16736f766ff45554a3b4b46eede7d3ef2b3d
zadnap <zadnap@users.noreply.github.com>
2024-08-12 21:07:16 +0700

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const sortedByAge = array.sort((person1, person2) => {
+        const currentYear = new Date().getFullYear()
+        const person1Age = (person1.yearOfDeath || currentYear) - person1.yearOfBirth
+        const person2Age = (person2.yearOfDeath || currentYear) - person2.yearOfBirth
 
+        return person1Age - person2Age
+    })
+    return sortedByAge[sortedByAge.length - 1]
 };
+
+findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ])
 
 // Do not edit below this line
 module.exports = findTheOldest;
