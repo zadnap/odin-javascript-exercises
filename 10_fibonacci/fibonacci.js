6910c947bbbc7e8bda0093288225417233824360
zadnap <zadnap@users.noreply.github.com>
2024-08-12 20:45:28 +0700

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index)
 
+    if (index < 0)
+        return 'OOPS'
+    if (index === 0)
+        return 0
+
+    let fibonacciSequence = [1, 1]
+
+    for (let i = 2; i < index; i++) {
+        fibonacciSequence.push(fibonacciSequence[i-1] + fibonacciSequence[i-2])
+    }
+
+    return fibonacciSequence[index - 1]
 };
 
 // Do not edit below this line
